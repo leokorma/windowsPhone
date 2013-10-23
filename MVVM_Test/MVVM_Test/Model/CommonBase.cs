7e15596b2d856dc81229f8f23ddac99917af8c04
leokorma <leokorma@gmail.com>
2013-10-23 10:39:40 +0300

    New MVVM example project

---
+++ MVVM_Test/MVVM_Test/Model/CommonBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace MVVM_Test.Model
+{
+    public class CommonBase : INotifyPropertyChanged 
+    {
+        /// <summary>
+        /// The PropertyChanged Event to raise to any UI object
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The PropertyChanged Event to raise to any UI object
+        /// The event is only invoked if data binding is used
+        /// </summary>
+        /// <param name="propertyName">The property name that is changing</param>
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+
+                // Raise the PropertyChanged event.
+                handler(this, args);
+            }
+        }
+    }
+}
